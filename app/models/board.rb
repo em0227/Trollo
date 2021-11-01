fe456ec6d7bf0c9805bf0df377597efc458066ea
Emily Wu <emilywu0227@gmail.com>
2021-11-01 16:29:36 -0400

    seeded db again

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ApplicationRecord
-    BG_COLORS = ['white', 'blue', 'velvet', 'yellow', 'ornage', 'green']
+    BG_COLORS = ['white', 'blue', 'velvet', 'yellow', 'orange', 'green']
 
     validates :title, :author_id, :bg_color, presence: true
     validates :bg_color, inclusion: BG_COLORS
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email: 'em0227new@aa.io', password: '123123', name: 'Emily');
+Board.create(title: 'First Board', bg_color: 'orange', author_id: 1);

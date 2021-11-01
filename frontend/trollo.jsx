3b6a4f97e567d6aaf0230e697096af841b30e31d
Emily Wu <emilywu0227@gmail.com>
2021-11-01 15:36:31 -0400

    added console.log testing

--- frontend/trollo.jsx
+++ frontend/trollo.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
-import { logout } from './actions/session_actions'
+import { logout } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // console.log("trollo.jsx");
+  console.log("trollo.jsx");
   // console.log('test');
   let store;
   if (window.currentUser) {

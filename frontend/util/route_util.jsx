2899e4d19ef5a59e292a3832839342ccc0e74de3
Emily Wu <emilywu0227@gmail.com>
2021-12-10 11:23:06 -0500

    mostly done for review

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import { Route, Redirect, withRouter } from 'react-router';
-import { connect } from 'react-redux'
+import React from "react";
+import { Route, Redirect, withRouter } from "react-router";
+import { connect } from "react-redux";
 
 const mapStateToProps = (state) => ({
-    loggedin: Boolean(state.session.id),
-    id: state.session.id
-})
+  loggedin: Boolean(state.session.id),
+  id: state.session.id,
+});
 
-const Auth = ({id, loggedin, path, exact, component: Component }) => (
-    <Route exact={exact} path={path} render={(props)=> (
-        loggedin ?  <Redirect to={`/users/${id}/boards`} /> : <Component {...props}/>
-    )} />
-)
+const Auth = ({ id, loggedin, path, exact, component: Component }) => (
+  <Route
+    exact={exact}
+    path={path}
+    render={(props) =>
+      loggedin ? (
+        <Redirect to={`/users/currentUser/boards`} />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
 
-const Protected = ({loggedin, path, exact, component: Component }) => (
-    <Route exact={exact} path={path} render={(props)=> (
-        loggedin ?  <Component {...props}/> : <Redirect to="/login"/>
-    )} />
-)
+const Protected = ({ loggedin, path, exact, component: Component }) => (
+  <Route
+    exact={exact}
+    path={path}
+    render={(props) =>
+      loggedin ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
 
-export const AuthRoute = withRouter(connect(mapStateToProps)(Auth))
-export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected))
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
